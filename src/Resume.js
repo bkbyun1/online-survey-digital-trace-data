0d7b8b37bc7993ed048bc11f1e2726b425f5b5c2
cyouh95 <cyouh95@g.ucla.edu>
2024-08-24 23:12:10 -0700

    prevent invalid response IDs from being recorded

--- src/Resume.js
+++ src/Resume.js
@@ -49,7 +49,9 @@ export default class Resume extends React.Component {
 		});
 
 		// Make sure document exists
-		this.db.collection('responseIDs').doc(this.props.qualtricsUserId + '_tier' + this.props.tierNumber).set({});
+		if (!this.props.qualtricsUserId.startsWith('${e:')) {
+			this.db.collection('responseIDs').doc(this.props.qualtricsUserId + '_tier' + this.props.tierNumber).set({});
+		}
 
 		// Track app start time
 		this.setState({
