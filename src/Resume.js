c49e5d2d1b3840517aa2fdc6c16360b5cfe4d655
bkbyun <bkbyun@umich.edu>
2024-09-22 08:47:40 -0400

    deleted brackets

--- src/Resume.js
+++ src/Resume.js
@@ -231,7 +231,7 @@ export default class Resume extends React.Component {
 					<div className="resume">
 						<div>
 							<div className="header">
-								<span style={{ fontWeight: "bold" }}>(Major: Engineering (Undeclared))</span>
+								<span style={{ fontWeight: "bold" }}>Major: Engineering (Undeclared)</span>
 							</div>
 							<div className="header" style={{ marginTop: '20px' }}>Student Information</div>
 							<p className="content" style={{ fontWeight: "bold" }}>Personal Information</p>
